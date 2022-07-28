4dae6a124609baaa2452384f594268be4a5873a1
MyrtheRijk <mjrijk@hotmail.com>
2022-07-29 00:53:15 +0200

    new project with navbar from bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # follow-your-dream
+
 A website where you can choose your dream and get to know how to follow your dream.
---
+++ follow-your-dream/src/components/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarComp from '../navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBarComp />
+    </div>
+  );
+}
+
+export default App;
